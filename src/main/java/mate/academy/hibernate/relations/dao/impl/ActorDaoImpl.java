f8b8743fa04ad378a6be4abf2d22098a017d5a6e
Nikita Kravtsov <deez999deez@gmail.com>
2026-04-20 15:45:22 +0300

    ADD: - hibernate and SQL dependencies to pom.xml - hibernate.cfg.xml with configuration
    IMPLEMENT:
    - HibernateUtil to create session factory
    - annotation to model classes to defined relation between entities:
      - Actor
      - Country
      - Movie
    - DAO implementation to add and get data to DB:
      - ActorDaoImpl
      - CountryDaoImpl
      - MovieDaoImpl
    - Service implementation:
      - ActorServiceImpl
      - CountryServiceImpl
      - MovieServiceImpl
    - Custom exception: DataProcessingException to throw in catch block on DAO
    - Initialize all service instances in Main.class main method;

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -2,8 +2,11 @@ package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class ActorDaoImpl extends AbstractDao implements ActorDao {
     public ActorDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,30 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
 
     @Override
     public Actor add(Actor actor) {
-        return null;
+        Transaction transaction = null;
+        Session session = factory.openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.persist(actor);
+            transaction.commit();
+            return actor;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Could not add actor: " + actor + " to DB", e);
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public Optional<Actor> get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            Actor actor = session.get(Actor.class, id);
+            return Optional.ofNullable(actor);
+        } catch (Exception e) {
+            throw new DataProcessingException("Could not get actor with id: " + id + " from DB", e);
+        }
     }
 }
